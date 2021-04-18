62ac562a04599ff4255c526159e64e93c98e210e
andreglegg <andre@fjong.com>
2021-04-18 12:44:53 +0200

    added reactstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import rootReducer from './reducers'
 import App from './App';
 
